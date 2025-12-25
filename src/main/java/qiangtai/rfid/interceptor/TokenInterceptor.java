3280f830c1d0cb6ff9ddc044275e8e5b3251edd3
三月柳絮 <zlf2074733059@126.com>
2025-12-25 08:54:15 +0800

    token响应优化

--- src/main/java/qiangtai/rfid/interceptor/TokenInterceptor.java
+++ src/main/java/qiangtai/rfid/interceptor/TokenInterceptor.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
-// 省略 import
 @Slf4j
 @Component
 public class TokenInterceptor implements HandlerInterceptor {
@@ -24,8 +23,6 @@ public class TokenInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) throws Exception {
-
-        String uri = request.getRequestURI();
 
         String token = request.getHeader(Constant.HEADER_TOKEN);
         if (token == null || !token.startsWith("Bearer ")) {
@@ -46,7 +43,7 @@ public class TokenInterceptor implements HandlerInterceptor {
 
         // 放入 ThreadLocal 上下文
         UserContext.set(new UserContext.UserInfo(userId, companyId));
-        log.error("【TokenInterceptor】ThreadLocal 已写入 userId={}", userId);
+        log.warn("【TokenInterceptor】ThreadLocal 已写入 userId={},companyId={}", userId,companyId);
         return true;
     }
 
@@ -56,7 +53,9 @@ public class TokenInterceptor implements HandlerInterceptor {
                                 Object handler,
                                 Exception ex) throws Exception {
         // 请求结束务必清理，防止线程复用导致串号
+
         UserContext.clear();
+        log.warn("【TokenInterceptor】ThreadLocal 已清理");
     }
 
 }
