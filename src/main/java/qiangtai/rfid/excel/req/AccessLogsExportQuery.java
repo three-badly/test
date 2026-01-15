c93a6a1b759be524e4f8122d4e0814d687daf0f0
16623 <zlf2074733059@qq.com>
2026-01-15 15:53:31 +0800

    fix: "设备模块"

--- src/main/java/qiangtai/rfid/excel/req/AccessLogsExportQuery.java
+++ src/main/java/qiangtai/rfid/excel/req/AccessLogsExportQuery.java
@@ -21,17 +21,17 @@ public class AccessLogsExportQuery extends PageRequest {
     @Schema(description = "员工手机号，留空则查全部")
     private String phoneNumber;
 
-    @Schema(description = "开始时间", example = "2025-01-01 00:00:00")
+    @Schema(description = "开始时间默认为很久以前，-999999999-01-01T00:00", example = "2025-01-01 00:00:00")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @NotNull(message = "开始时间不能为空")
-    private LocalDateTime startTime;
+    private LocalDateTime startTime = LocalDateTime.MIN;
 
-    @Schema(description = "结束时间", example = "2025-12-31 23:59:59")
+    @Schema(description = "结束时间默认为当前时间", example = "2025-12-31 23:59:59")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @NotNull(message = "结束时间不能为空")
-    private LocalDateTime endTime;
+    private LocalDateTime endTime = LocalDateTime.now();
 
     @Schema(description = "部门名称，留空则查全部")
     private String deptName;
